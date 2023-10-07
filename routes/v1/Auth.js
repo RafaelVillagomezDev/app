ae240cc50d1944e5d8cd9a9fe3f3e5816d8c52d0
rafael.dev <rafaelvillagomez.dev@gmail.com>
2023-10-07 18:46:30 +0200

    Descomentar getSurvey

--- routes/v1/Auth.js
+++ routes/v1/Auth.js
@@ -4,6 +4,6 @@ const authController=require("../../controllers/authController")
 const validateAuth=require("../../validators/auth")
 
 router.post('/register', validateAuth("create"),authController.registerAuthUser);
-router.get('/login', validateAuth("login"),authController.loginAuthUser);
+router.post('/login', validateAuth("login"),authController.loginAuthUser);
 
 module.exports=router
